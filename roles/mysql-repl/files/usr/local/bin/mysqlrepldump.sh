8e0c7e71e6349181d6ac8ff533ed053528b38e84
akagisho <akagisho@gmail.com>
2014-08-19 12:41:04 +0900

    Fix task

--- roles/mysql-repl/files/usr/local/bin/mysqlrepldump.sh
+++ roles/mysql-repl/files/usr/local/bin/mysqlrepldump.sh
@@ -5,7 +5,7 @@ set -e
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 CONF=/etc/mysql/my.cnf
 CMD=mysqldump
-ARG="$@ --single-transaction --master-data=2 --databases"
+ARG="$@ --single-transaction --master-data=1 --databases"
 
 for DB in `grep ^binlog-do-db $CONF | awk '{print $3}'`; do
   ARG="$ARG $DB"
